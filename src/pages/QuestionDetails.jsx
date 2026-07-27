179aca5f64661624892bd3fe8300c7789cf46ad8
deepgit-hub <darkdeepak001@gmail.com>
2026-07-27 22:19:53 +0530

    added questiondetils

---
+++ src/pages/QuestionDetails.jsx
@@ -0,0 +1,60 @@
+import { useNavigate, useParams } from "react-router-dom";
+import questions from "../data/question";
+
+function QuestionDetails() {
+  const navigate = useNavigate();
+
+  const { topicId, questionId } = useParams();
+
+  const topic = questions.find(
+    (item) => item.topicId === Number(topicId)
+  );
+
+  if (!topic) {
+    return <h2>Topic Not Found</h2>;
+  }
+
+  const question = topic.questions.find(
+    (item) => item.id === Number(questionId)
+  );
+
+  if (!question) {
+    return <h2>Question Not Found</h2>;
+  }
+
+  return (
+    <>
+      <button onClick={() => navigate(`/question/${topicId}`)}>
+        ⬅ Back
+      </button>
+
+      <h1>{topic.title}</h1>
+
+      <h2>{question.title}</h2>
+
+      <hr />
+
+      <h2>📝 Question (English)</h2>
+
+      <p>{question.questionEnglish}</p>
+
+      <hr />
+
+      <h2>📝 கேள்வி (Tamil)</h2>
+
+      <p>{question.questionTamil}</p>
+
+      <hr />
+
+      <button>💻 Run Code</button>
+
+      {!question.challenge && (
+        <button>📖 Solution</button>
+      )}
+
+      <button>✅ Done</button>
+    </>
+  );
+}
+
+export default QuestionDetails;
