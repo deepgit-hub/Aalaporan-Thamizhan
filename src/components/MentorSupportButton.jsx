1c500c773b433e4cbd6e4ac9f48c9193cb1cd86c
deepgit-hub <darkdeepak001@gmail.com>
2026-07-31 23:37:26 +0530

    x

--- src/components/MentorSupportButton.jsx
+++ src/components/MentorSupportButton.jsx
@@ -3,7 +3,7 @@ function MentorSupport() {
   const handleClick = () => {
 
     window.location.href =
-      "mailto:yourprofessor@gmail.com?subject=Alaporan Tamilan - Mentor Support&body=Hello Sir,%0D%0A%0D%0AI need help regarding:%0D%0A%0D%0ATopic:%0D%0AQuestion:%0D%0AMy Doubt:%0D%0A%0D%0AThank You.";
+      "mailto:deepak2911001@gmail.com?subject=Alaporan Tamilan - Mentor Support&body=Hello Sir,%0D%0A%0D%0AI need help regarding:%0D%0A%0D%0ATopic:%0D%0AQuestion:%0D%0AMy Doubt:%0D%0A%0D%0AThank You.";
 
   };
 
