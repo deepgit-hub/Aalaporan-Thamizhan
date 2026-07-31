bcfd1e928c35be31b8aa32b6d0e1e054df20ae18
deepgit-hub <darkdeepak001@gmail.com>
2026-07-31 22:18:47 +0530

    ds

--- src/pages/QuestionDetails.jsx
+++ src/pages/QuestionDetails.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import questions from "../data/question";
-
+import "./styles/QuestionDetails.css";
 
 function QuestionDetails() {
   const navigate = useNavigate();
