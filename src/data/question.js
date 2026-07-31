df789d56995022cf373ae058aa809c460eabb8ce
deepgit-hub <darkdeepak001@gmail.com>
2026-07-31 14:25:58 +0530

    w

--- src/data/question.js
+++ src/data/question.js
@@ -13494,70 +13494,9 @@ Designing Software = Figma`,
 },
     ],
   },
+  
   {
     topicId: 31,
-    title: "Multiple Inheritance",
-
-    questions: [
-      {
-        id: 1,
-        title: "Question 1",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Question 2",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 3,
-        title: "Question 3",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 4,
-        title: "Question 4",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 5,
-        title: "Question 5",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 6,
-        title: "Question 6",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 7,
-        title: "Question 7",
-        locked: false,
-        completed: false,
-      },
-      {
-        id: 8,
-        title: "Question 8",
-        locked: false,
-        completed: false,
-      },
-      { id: 9, title: "Challenge Question 9", locked: true, completed: false },
-      {
-        id: 10,
-        title: "Challenge Question 10",
-        locked: true,
-        completed: false,
-      },
-    ],
-  },
-  {
-    topicId: 32,
     title: "Hybrid Inheritance",
 
     questions: [
@@ -13619,7 +13558,7 @@ Designing Software = Figma`,
     ],
   },
   {
-    topicId: 33,
+    topicId: 32,
     title: "Exception Handling",
 
     questions: [
@@ -13681,7 +13620,7 @@ Designing Software = Figma`,
     ],
   },
   {
-    topicId: 34,
+    topicId: 33,
     title: "Try Catch",
 
     questions: [
