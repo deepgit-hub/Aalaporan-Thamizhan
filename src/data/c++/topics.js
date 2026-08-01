f2ad4e0b60102c1ffcb1d361d472a8154123c772
deepgit-hub <darkdeepak001@gmail.com>
2026-08-01 19:33:54 +0530

    d

--- src/data/c++/topics.js
+++ src/data/c++/topics.js
@@ -1,3 +1,46 @@
-const topics = [];
+const topics = [
+    {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+];
 
 export default topics;
