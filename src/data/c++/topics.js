4218fe3e54a67f6b6127e51849ba89cd690e3cab
deepgit-hub <darkdeepak001@gmail.com>
2026-08-01 19:35:09 +0530

    d

--- src/data/c++/topics.js
+++ src/data/c++/topics.js
@@ -41,6 +41,805 @@ const topics = [
 
   completed: false,
 },
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+  {
+  id: 1,
+
+  title: "",
+
+  englishDefinition:
+    "",
+
+  tamilDefinition:
+    "",
+
+  realWorldUsage:
+    "",
+  realWorldUsageTamil:
+    "",
+  
+    syntax: ``,
+
+  syntaxExplanationEnglish: [
+   
+  ],
+
+  syntaxExplanationTamil: [
+    
+  ],
+
+  exampleProgram: ``,
+
+  programExplanationEnglish: [
+    
+  ],
+
+  programExplanationTamil: [
+    
+  ],
+
+  output: ``,
+
+  locked: false,
+
+  completed: false,
+},
+
 ];
 
 export default topics;
