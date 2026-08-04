0009d254fe6ee907e4e8c35676c168077b7ad55c
deepgit-hub <darkdeepak001@gmail.com>
2026-08-04 17:40:53 +0530

    cs

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAStzy7krxiv2ZDGRG-9ZDVjKNfG0j0DqY",
@@ -11,4 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export const db = getFirestore(app);
