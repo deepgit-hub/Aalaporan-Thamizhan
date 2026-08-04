66b6c2ee97e3f3526675f05b8bbde21b4c888941
deepgit-hub <darkdeepak001@gmail.com>
2026-08-04 18:05:11 +0530

    cs

--- src/pages/Languages.jsx
+++ src/pages/Languages.jsx
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 function Languages() {
+  const [languages, setLanguages] = useState([]);
+
   useEffect(() => {
     async function fetchLanguages() {
       const snapshot = await getDocs(collection(db, "languages"));
 
-      snapshot.forEach((doc) => {
-        console.log(doc.id, doc.data());
-      });
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(data);
+      setLanguages(data);
     }
 
     fetchLanguages();
@@ -18,6 +23,12 @@ function Languages() {
   return (
     <div>
       <h1>Languages</h1>
+
+      {languages.map((language) => (
+        <h2 key={language.id}>
+          {language.name}
+        </h2>
+      ))}
     </div>
   );
 }
