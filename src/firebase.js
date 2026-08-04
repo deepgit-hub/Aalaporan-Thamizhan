8ccbc18bad8a0c15c0021c400ad64a90a16b4402
deepgit-hub <darkdeepak001@gmail.com>
2026-08-04 16:15:21 +0530

    dw

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAStzy7krxiv2ZDGRG-9ZDVjKNfG0j0DqY",
+  authDomain: "aalaporan-thamizhan.firebaseapp.com",
+  projectId: "aalaporan-thamizhan",
+  storageBucket: "aalaporan-thamizhan.firebasestorage.app",
+  messagingSenderId: "727449277716",
+  appId: "1:727449277716:web:fb1620273d4779502e9e01"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
