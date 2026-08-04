f2a0b65802aaad2a342ce53b187f14696ce08a2f
deepgit-hub <darkdeepak001@gmail.com>
2026-08-05 00:23:07 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 
 import Languages from "./pages/Languages";
-import Topics from "./pages/Topic";
-import Questions from "./pages/Question";
+import Home from "./pages/Home";
+import Topic from "./pages/Topic";
+import Question from "./pages/Question";
 import QuestionDetails from "./pages/QuestionDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Languages />} />
-      <Route path="/topics/:languageId" element={<Topics />} />
-      <Route path="/questions/:languageId/:topicId" element={<Questions />} />
+
+      <Route
+        path="/home/:languageId"
+        element={<Home />}
+      />
+
+      <Route
+        path="/topic/:languageId/:topicId"
+        element={<Topic />}
+      />
+
+      <Route
+        path="/questions/:languageId/:topicId"
+        element={<Question />}
+      />
+
       <Route
         path="/question/:languageId/:topicId/:questionId"
         element={<QuestionDetails />}
