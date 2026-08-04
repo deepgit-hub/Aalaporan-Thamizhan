a53e4dee8bd774b3a91a20607fb7973b978d5650
deepgit-hub <darkdeepak001@gmail.com>
2026-08-04 20:35:15 +0530

    c

--- src/pages/Topic.jsx
+++ src/pages/Topic.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 function Topic() {
   const { languageId } = useParams();
 
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    async function fetchTopics() {
+      const snapshot = await getDocs(
+        collection(db, "languages", languageId, "concepts")
+      );
+
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setTopics(data);
+    }
+
+    fetchTopics();
+  }, [languageId]);
+
   return (
     <div>
-      <h1>Topics</h1>
-      <h2>{languageId}</h2>
+      <h1>{languageId}</h1>
+
+      {topics.map((topic) => (
+        <h2 key={topic.id}>{topic.title}</h2>
+      ))}
     </div>
   );
 }
