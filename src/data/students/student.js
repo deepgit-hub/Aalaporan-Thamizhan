165a734e9447e5084096d98f4ac8db93a92666d6
deepgit-hub <darkdeepak001@gmail.com>
2026-08-06 00:23:51 +0530

    d

---
+++ src/data/students/student.js
@@ -0,0 +1,39 @@
+const students = [
+  {
+    username: "B24101",
+
+    password: "B24101",
+
+    name: "AAKASH G",
+
+    language: "java",
+
+    completedTopics: [],
+  },
+
+  {
+    username: "B24102",
+
+    password: "B24102",
+
+    name: "ABINA A",
+
+    language: "cpp",
+
+    completedTopics: [],
+  },
+
+  {
+    username: "B24103",
+
+    password: "B24103",
+
+    name: "ABISHEK A",
+
+    language: "python",
+
+    completedTopics: [],
+  },
+];
+
+export default students;
