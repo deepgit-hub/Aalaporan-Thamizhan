7449811a34e1b7fd9f84de8296ff58cc6f87338b
deepgit-hub <darkdeepak001@gmail.com>
2026-08-06 01:58:29 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,15 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Languages />} />
+    <Route path="/" element={<Login />} />
+    <Route path="/languages" element={<Languages />} />
+      <Route path="/home/:languageId" element={<Home />}/>
 
-      <Route
-        path="/home/:languageId"
-        element={<Home />}
-      />
+      <Route path="/topic/:languageId/:topicId" element={<Topic />} />
 
-      <Route
-        path="/topic/:languageId/:topicId"
-        element={<Topic />}
-      />
+      <Route path="/questions/:languageId/:topicId" element={<Question />} />
 
-      <Route
-        path="/questions/:languageId/:topicId"
-        element={<Question />}
-      />
-
-      <Route
-        path="/question/:languageId/:topicId/:questionId"
-        element={<QuestionDetails />}
-      />
+      <Route path="/question/:languageId/:topicId/:questionId" element={<QuestionDetails />}/>
     </Routes>
   );
 }
