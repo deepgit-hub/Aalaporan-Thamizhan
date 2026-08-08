f9a3e10c8fe71b1473da776b29b61bd299fa5969
deepgit-hub <darkdeepak001@gmail.com>
2026-08-08 09:16:44 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Languages from "./pages/Languages";
 import Home from "./pages/Home";
 import Topic from "./pages/Topic";
@@ -6,19 +7,60 @@ import Question from "./pages/Question";
 import QuestionDetails from "./pages/QuestionDetails";
 import Login from "./pages/Login";
 import Welcome from "./pages/Welcome";
+import ChooseLanguage from "./pages/ChooseLanguage";
+
 function App() {
   return (
     <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/welcome" element={<Welcome />}/>
-    <Route path="/languages" element={<Languages />} />
-      <Route path="/home/:languageId" element={<Home />}/>
 
-      <Route path="/topic/:languageId/:topicId" element={<Topic />} />
+      {/* Login */}
+      <Route
+        path="/"
+        element={<Login />}
+      />
 
-      <Route path="/questions/:languageId/:topicId" element={<Question />} />
+      {/* Choose Language */}
+      <Route
+        path="/choose-language"
+        element={<ChooseLanguage />}
+      />
 
-      <Route path="/question/:languageId/:topicId/:questionId" element={<QuestionDetails />}/>
+      {/* Welcome */}
+      <Route
+        path="/welcome"
+        element={<Welcome />}
+      />
+
+      {/* Languages */}
+      <Route
+        path="/languages"
+        element={<Languages />}
+      />
+
+      {/* Home */}
+      <Route
+        path="/home/:languageId"
+        element={<Home />}
+      />
+
+      {/* Topic */}
+      <Route
+        path="/topic/:languageId/:topicId"
+        element={<Topic />}
+      />
+
+      {/* Questions */}
+      <Route
+        path="/questions/:languageId/:topicId"
+        element={<Question />}
+      />
+
+      {/* Question Details */}
+      <Route
+        path="/question/:languageId/:topicId/:questionId"
+        element={<QuestionDetails />}
+      />
+
     </Routes>
   );
 }
